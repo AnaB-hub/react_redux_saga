e5bf1121b457f6bfb6442ffb6815cf6d435eae88
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 09:03:35 -0300

    exemplo sem typescript

---
+++ reduxsaga/src/store/ducks/repositories/types.ts
@@ -0,0 +1,16 @@
+/**
+ * Actions types
+ */
+const Types = {
+  LOAD_REQUEST: "repositories/LOAD_REQUEST",
+  LOAD_SUCCESS: "repositories/LOAD_SUCCESS",
+  LOAD_FAILURE: "repositories/LOAD_FAILURE",
+};
+
+/**
+ * Reducer
+ */
+
+/**
+ * Action creators
+ */
