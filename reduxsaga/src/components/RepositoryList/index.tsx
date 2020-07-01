ea4674347685fff7c3a82aa040669e307154d981
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 08:10:14 -0300

    Realiza a tipagem do state e props em RepositoryList

---
+++ reduxsaga/src/components/RepositoryList/index.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+interface Repository {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  repositories: Repository[];
+}
+
+interface State {
+  newRepository?: string
+}
+
+export default class RepositoryList extends Component<Props, State> {
+  state = {
+    newRepository: "",
+  };
+
+  ComponentDidMount() {}
+
+  render() {
+    const { repositories } = this.props;
+    const { newRepository } = this.state;
+
+    // newRepository.toLowerCase();
+
+    return (
+      <ul>
+        {repositories.map(repository => <li>{repository.}</li>)}
+      </ul>
+    )
+  }
+}
