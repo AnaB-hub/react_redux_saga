92f3bbfe5115e227551f5378c84f4fb7a9221d01
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 09:04:03 -0300

    Adiciona dependências e arquivos para o redux

--- reduxsaga/src/components/RepositoryList/index.tsx
+++ reduxsaga/src/components/RepositoryList/index.tsx
@@ -1,37 +1,11 @@
 import React, { Component } from "react";
 
-interface Repository {
-  id: number;
-  name: string;
-}
-
-interface Props {
-  repositories: Repository[];
-}
-
-interface State {
-  newRepository?: string;
-}
-
-export default class RepositoryList extends Component<Props, State> {
-  state = {
-    newRepository: "",
-  };
-
+export default class RepositoryList extends Component {
   ComponentDidMount() {}
 
   render() {
-    const { repositories } = this.props;
-    const { newRepository } = this.state;
-
     // newRepository.toLowerCase();
 
-    return (
-      <ul>
-        {repositories.map((repository) => (
-          <li>{repository.id}</li>
-        ))}
-      </ul>
-    );
+    return <ul></ul>;
   }
 }
